7f9677c2a048c7a1bf948b8affceee059cb7a387
AnthonyDally72 <Anthony.Dally72@outlook.com>
2020-05-11 22:13:29 -0400

    changes to Producttypes view/Model

---
+++ BikeStore/BikeStore/Areas/Admin/Controllers/ProductTypesController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BikeStore.Data;
+using BikeStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BikeStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductTypesController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public ProductTypesController( ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View(_db.ProductTypes.ToList());
+        }
+
+        // Get Create Action Method
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        // Post Create action method
+        [HttpPost]
+        [ValidateAntiForgeryToken] // checks whether the token is valid or not. 
+        public async Task<IActionResult> Create(ProductTypes productTypes)
+        {
+            if (ModelState.IsValid) //  checks the requirements of the model properties
+            {
+                _db.Add(productTypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productTypes);
+        }
+
+
+        // Get Edit Action Method
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id==null)
+            {
+                return NotFound();
+
+            }
+
+            var productType = await _db.ProductTypes.FindAsync(id);
+            if(productType == null)
+            {
+                return NotFound();
+            }
+
+            return View(productType);
+            
+        }
+
+
+        // Post Edit action method
+        [HttpPost]
+        [ValidateAntiForgeryToken] // checks whether the token is valid or not. 
+        public async Task<IActionResult> Edit(int id, ProductTypes productTypes)
+        {
+            if (id != productTypes.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid) //  checks the requirements of the model properties
+            {
+                _db.Update(productTypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productTypes);
+        }
+        // GET Delete Action Method
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+
+            }
+
+            var productType = await _db.ProductTypes.FindAsync(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+
+            return View(productType);
+
+        }
+
+        // Get Delete Action Method
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+
+            }
+
+            var productType = await _db.ProductTypes.FindAsync(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+
+            return View(productType);
+
+        }
+
+
+        // Post Delete action method
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken] // checks whether the token is valid or not. 
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var productTypes = await _db.ProductTypes.FindAsync(id);
+            _db.ProductTypes.Remove(productTypes);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
+            
+        }
+
+
+    }
+}
--- BikeStore/BikeStore/Models/Product.cs
+++ BikeStore/BikeStore/Models/Product.cs
@@ -2,17 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
 
 namespace BikeStore.Models
 {
-    public class ProductTypes
+    public class Product
     {
-        public int Id { get; set; }
-
-        [Required]
-        public string Name { get; set; }
-
-
     }
 }
